2a190487e0510c6bee023f6a7c3093b975df9966
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-13 02:04:57 +0600

    part 01 complete

--- varriable.c
+++ varriable.c
@@ -1,6 +1,5 @@
 // #include <stdio.h>
 
-
 // int main()
 // {
 //     // int n = 5; //initialization
@@ -15,18 +14,18 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
+// int main()
+// {
 
-    int x, y;
-    int ch;
-    scanf("%d %d %c", &x, &y, &ch);
-    printf("Output x: %d\nOutput y: %d\nCharacter z: %c\n", x,y,ch);
+//     int x, y;
+//     int ch;
+//     scanf("%d %d %c", &x, &y, &ch);
+//     printf("Output x: %d\nOutput y: %d\nCharacter z: %c\n", x,y,ch);
 
-    return 0;
-}
+//     return 0;
+// }
 // #include <stdio.h>
 
 // int main()
@@ -38,3 +37,139 @@ int main()
 
 //     return 0;
 // }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x = 100000;
+//     int y = 100000;
+
+//     long long result = x * (y * 1LL);
+//     printf("%lld", result);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x = 100000;
+//     double y = 100000;
+
+//     long long result = x * y;
+//     printf("%lld", result);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     if (5 < 6 && 3 < 5)
+//     {
+//         printf("gotona sotti!\n");
+//     }
+//     else
+//     {
+//         printf("gotona sotti noi\n");
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     if (5 > 6 || 3 > 5)
+//     {
+//         printf("gotona sotti!\n");
+//     }
+//     else
+//     {
+//         printf("gotona sotti noi");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     if (5 < 6 && 3 < 5)
+//     {
+//         printf("gotona sotti!\n");
+//     }
+//     else if(3 != 2 && 3 > 2)
+//     {
+//         printf("Uporer gotona mitta\n majer gotona sotto");
+//     }
+//     else
+//     {
+//         printf("gotona sotti noi\n");
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     if (5 < 6 && 3 > 5)
+//     {
+//         printf("gotona sotti!\n");
+//     }
+//     else if(3 != 2 && 3 > 2)
+//     {
+//         printf("Uporer gotona mitta\n majer gotona sotto");
+//     }
+//     else
+//     {
+//         printf("gotona sotti noi\n");
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     if (5 < 6 && 3 > 5)
+//     {
+//         printf("gotona sotti!\n");
+//     }
+//     else if(3 != 3 && 3 > 2)
+//     {
+//         printf("Uporer gotona mitta\n majer gotona sotto");
+//     }
+//     else
+//     {
+//         printf("gotona sotti noi\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int opt;
+
+    scanf("%d", &opt);
+
+    switch (opt)
+    {
+    case 1:
+        printf("One");
+        break;
+    case 2:
+        printf("Two");
+        break;
+    case 3:
+        printf("Three");
+        break;
+    
+    default:
+        printf("invalid number");
+    }
+
+    return 0;
+}

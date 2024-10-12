1f2ce1d8191a816f6fb8adc29e3ec54091d0d14d
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-12 18:19:16 +0600

    first commit

---
+++ data_type.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    int a = 5;
+    float b = 5.61;
+    int ab = 56.63;
+    char c = '1';
+    bool d = true;
+
+    return 0;
+}
---
+++ varriable.c
@@ -0,0 +1,40 @@
+// #include <stdio.h>
+
+
+// int main()
+// {
+//     // int n = 5; //initialization
+//     // int a;     //declaration
+//     // a = 15;    // assignment
+
+//     int x, y;
+//     scanf("%d %d", &x, &y);
+//     printf("Output x: %d\n", x);
+//     printf("Output y: %d\n", y);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+
+    int x, y;
+    int ch;
+    scanf("%d %d %c", &x, &y, &ch);
+    printf("Output x: %d\nOutput y: %d\nCharacter z: %c\n", x,y,ch);
+
+    return 0;
+}
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int x, y;
+//     scanf("%d %d", &x, &y);
+//     printf("Output x: %d\nOutput y: %d\n", x,y);
+
+//     return 0;
+// }

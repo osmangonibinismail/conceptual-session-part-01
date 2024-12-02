b9368b55dc152596a4b92aa519821ac93d2b9a6c
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-12-02 17:10:50 +0600

    repeat solve

--- data_type.c
+++ data_type.c
@@ -1,13 +1,109 @@
-#include<stdio.h>
-#include<stdbool.h>
+// #include<stdio.h>
+// #include<stdbool.h>
 
-int main()
-{
-    int a = 5;
-    float b = 5.61;
-    int ab = 56.63;
-    char c = '1';
-    bool d = true;
+// int main()
+// {
+//     int a = 5;
+//     float b = 5.61;
+//     int ab = 56.63;
+//     char c = '1';
+//     bool d = true;
 
-    return 0;
-}
+//     return 0;
+// }
+
+// #include<stdio.h>
+
+// struct student {
+//     int roll;
+//     float weight;
+// };
+
+// int main()
+// {
+//     struct student s[10];
+
+//     s[0]. roll = 11;
+//     s[0].weight = 60.6;
+
+//     printf("%d %f", s[0].roll, s[0].weight);
+// }
+// #include <stdio.h>
+
+// struct student
+// {
+//     int roll;
+//     float weight;
+// };
+
+// int main()
+// {
+//     struct student s[10];
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         s[i].roll = 3001 + i;
+//         s[i].weight = 60.6 + i * 2;
+//     }
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("Student ID-> %d  Weight-> %f\n", s[0].roll, s[0].weight);
+//     }
+// }
+// #include <stdio.h>
+
+// struct student
+// {
+//     int roll;
+//     float weight;
+// };
+
+// int main()
+// {
+//     struct student s[10];
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         s[i].roll = 3001 + i;
+//         s[i].weight = 60.6 + i * 2;
+//     }
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("Student ID-> %d  Weight-> %f\n", s[0].roll, s[0].weight);
+//     }
+// }
+
+// #include <stdio.h>
+
+// struct student
+// {
+//     int roll;
+//     float weight;
+// };
+
+// int main()
+// {
+//     struct student s = {.roll = 30, .weight = 80.4};
+
+//     struct student *sp;
+//     sp = &s;
+
+//     printf("Student ID-> %d  Weight-> %f\n", sp->roll, sp->weight);
+// }
+// #include <stdio.h>
+
+// struct student
+// {
+//     int roll;
+//     float weight;
+// };
+
+// int main()
+// {
+//     struct student s = {.roll = 30, .weight = 80.4};
+
+//     struct student *sp;
+//     sp = &s;
+
+//     printf("Student ID-> %d  Weight-> %f\n", sp->roll, sp->weight);
+// }
---
+++ repeat.c
@@ -0,0 +1,27 @@
+// #include<stdio.h>
+
+// int main()
+// {
+//     int x, y;
+//     scanf("%d %d", &x, &y);
+
+//     printf("Value of X: %d\nValue of Y: %d", x, y);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int x;
+    int y;
+
+    printf("Enter number of X: ");
+    scanf("%d", &x);
+    printf("Enter number of Y: ");
+    scanf("%d", &y);
+
+    printf("Value of X: %d\n", x);
+    printf("Value of Y: %d\n", y);
+    return 0;
+}

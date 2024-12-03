1b6f99e9fb36fc7b5653aafd8bae84ad15ca5b63
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-12-03 08:59:02 +0600

    revise and practice

--- repeat.c
+++ repeat.c
@@ -10,18 +10,93 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//     int x;
+//     int y;
+
+//     printf("Enter number of X: ");
+//     scanf("%d", &x);
+//     printf("Enter number of Y: ");
+//     scanf("%d", &y);
+
+//     printf("Value of X: %d\n", x);
+//     printf("Value of Y: %d\n", y);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main()
+// {
+//     int x;
+//     char y;
+
+//     printf("Enter number of X: ");
+//     scanf("%d%%", &x);
+//     printf("Enter number of Y: ");
+//     getchar();
+//     scanf("%c", &y);
+
+//     printf("Value of X: %d\n", x);
+//     printf("Value of Y: %c\n", y);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int x = 10;
+//     printf("%d\n", x++);
+//     printf("%d\n", x);
+
+//     return 0;
+
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int A = 10;
+
+//     printf("%d\n", A--);
+
+//     printf("%d\n", A);
+
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int A = 10;
+
+//     printf("%d\n",--A);
+
+//     printf("%d\n", ++A);
+
+//     return 0;
+// }
+#include<stdio.h>
 int main()
 {
-    int x;
-    int y;
+    int A = 10;
+    printf("%d\n", A);
 
-    printf("Enter number of X: ");
-    scanf("%d", &x);
-    printf("Enter number of Y: ");
-    scanf("%d", &y);
+    int X = A++;
+    printf("%d\n", X);
 
-    printf("Value of X: %d\n", x);
-    printf("Value of Y: %d\n", y);
+    --X;
+    printf("%d\n", X);
+    A--;
+    printf("%d\n", A);
+
+    int y = --X;
+    printf("%d\n", y);
+
+    printf("%d\n",y--);
+    printf("%d\n", y);
+
+
     return 0;
 }

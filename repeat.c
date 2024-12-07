c7087d4585dc5e8f51201689f2187918a25707fc
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-12-07 12:40:30 +0600

    nested lop pattern problem revise

--- repeat.c
+++ repeat.c
@@ -246,24 +246,54 @@
 //     while(i<10);
 // }
 
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
 
-    int count = 0;
+//     int count = 0;
 
-    for(int i = 1; i <= n; i++){
-        if(i % 2 == 0){
+//     for(int i = 1; i <= n; i++){
+//         if(i % 2 == 0){
 
-            count = count + 1;
-            printf("%d\n",i);
-        }
-    }
-    if(count == 0){
-        printf("-1");
-    }
+//             count = count + 1;
+//             printf("%d\n",i);
+//         }
+//     }
+//     if(count == 0){
+//         printf("-1");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     for(int i = 1; i <= n; i++){
+//         printf("%d\n",i);
+//     }
+// }
+
+// #include<stdio.h>
+
+// int main(){
+
+//     int n;
+//     scanf("%d", &n);
+
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j <= i; j++)
+//         {
+//             printf("* ");
+//         }
+//             printf("\n");
+//     }
+// }
+
